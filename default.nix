d847059d869ae8ece42d87708957fc3ddad908ad
Shea Levy <shea@shealevy.com>
2018-11-18 17:22:30 -0500

    Initial scaffolding to get to git.

---
+++ default.nix
@@ -0,0 +1,32 @@
+let
+  pkgs = import <nixpkgs> {}; # TODO pin
+  desktop-tools = import /home-persistent/shlevy/src/shlevy-desktop-tools pkgs;
+  emacs = pkgs.emacsWithPackages (epkgs: [ epkgs.exwm epkgs.notmuch epkgs.znc epkgs.magit ]);
+  xsession = pkgs.substituteAll {
+    src = ./xsession;
+    isExecutable = true;
+    inherit (pkgs) bash firefox pass gnupg isync msmtp git;
+    inherit (pkgs.emacsPackages) notmuch;
+    inherit (pkgs.xorg) xhost;
+    movemail = desktop-tools.move-mail;
+    mailloop = desktop-tools.mail-loop;
+    inherit emacs;
+  };
+in ((pkgs.callPackage ./symlink-tree.nix {}).compose {
+  requires = {};
+  provides.name = "shlevy-home";
+  provides.links = {
+    creds = "/home-persistent/shlevy/creds";
+    ".xsession" = xsession;
+    ".mbsyncrc" = ./dotfiles/mbsyncrc;
+    ".msmtprc" = ./dotfiles/msmtprc;
+    ".notmuch-config" = ./dotfiles/notmuch-config;
+    ".emacs" = ./dotfiles/emacs;
+    ".mozilla" = "/home-persistent/shlevy/xdg/config/mozilla";
+    ".xsession-errors" = "run/xsession-errors";
+    ".Xauthority" = "run/Xauthority";
+    ".cache/mozilla" = "/home-persistent/shlevy/xdg/cache/mozilla";
+    ".gitconfig" = ./dotfiles/gitconfig;
+    run = "/run/user/1000";
+  };
+}).provides.run
---
+++ symlink-tree.nix
@@ -0,0 +1,12 @@
+{ runCommand }:
+{
+  compose = { provides, requires }: {
+    provides.run = let
+      snippets = map (name: ''
+        link_name="$out/${name}"
+        mkdir --parents "$(dirname "$link_name")"
+	ln --symbolic --no-target-directory ${provides.links.${name}} "$link_name"
+      '') (builtins.attrNames provides.links);
+    in runCommand provides.name {} (builtins.concatStringsSep "\n" snippets);
+  };
+}
