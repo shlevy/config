8e918741a4214a2be053cfc2423cc819f3574106
Shea Levy <shea@shealevy.com>
2019-01-13 09:51:29 -0500

    Bump nix-mode.

--- nix.nix
+++ nix.nix
@@ -1,11 +1,19 @@
-{ nix }:
-{
+{ nix, fetchFromGitHub }: let
+  nix-mode-src = fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nix-mode";
+    rev = "80a1e96c7133925797a748cf9bc097ca6483baeb";
+    sha256 = "165g8ga1yhibhabh6n689hdk525j00xw73qnsdc98pqzsc2d2ipa";
+  };
+in {
   compose = { requires, provides }: {
     requires.links.".local/share/nix" = "/home-persistent/shlevy/xdg/share/nix";
 
     requires.links.".cache/nix" = "/home-persistent/shlevy/xdg/cache/nix";
 
-    requires.emacs-package = epkgs: epkgs.nix-mode;
+    requires.emacs-package = epkgs: epkgs.nix-mode.overrideAttrs (_: {
+      src = nix-mode-src;
+    });
 
     requires.emacs-config = "(setq nix-indent-function 'nix-indent-line)";
 
