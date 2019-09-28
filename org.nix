6591ec8f4ba6bbf9312f5338b775c72016896277
Shea Levy <shea@shealevy.com>
2019-09-28 09:16:08 -0400

    More agenda.

--- org.nix
+++ org.nix
@@ -3,7 +3,7 @@
     requires.emacs-package = epkgs: epkgs.org-plus-contrib;
 
     requires.emacs-config = ''
-      (setq org-agenda-files '("~/documents/agenda.org" "~/documents/intention-tracking.org"))
+      (setq org-agenda-files '("~/documents/agenda.org" "~/documents/intention-tracking.org" "~/documents/financial/receipts-processing.org" "~/documents/currently-reading.org"))
       (setq org-enforce-todo-dependencies t)
     '';
   };
