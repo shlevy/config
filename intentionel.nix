47c635d73e5e3c2d9476c0060a78c8b736e9b8ec
Shea Levy <shea@shealevy.com>
2019-10-27 07:21:37 -0400

    Intentionel bump

--- intentionel.nix
+++ intentionel.nix
@@ -2,7 +2,7 @@
   compose = { requires, provides }: {
     requires.emacs-package = epkgs: epkgs.elpaBuild {
       pname = "intentionel";
-      version = "1.0.0";
+      version = "1.0.1";
       src = /home/shlevy/src/intentionel/intentionel.el;
       packageRequires = [
         epkgs.org
