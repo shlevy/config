c2a2988e5e1cd45dc76b50f0aea7554f92ed83b4
Shea Levy <shea@shealevy.com>
2022-10-01 04:35:34 -0400

    Cachix and autobuild

--- flake.nix
+++ flake.nix
@@ -8,15 +8,19 @@
   };
   outputs = { self, nixpkgs, ... }@inputs: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    inherit (pkgs) writeShellScript nixos-rebuild gnugrep;
   in {
     apps.x86_64-linux.default = {
       type = "app";
       program = (pkgs.writeShellScript "rebuild" ''
         set -eEuo pipefail
 
+        readonly grep="${gnugrep}/bin/grep"
+        readonly nixos_rebuild="${nixos-rebuild}/bin/nixos-rebuild"
+
         cmd="''${1:-switch}"
 
-        if ! grep --quiet --no-messages --fixed-strings 102363c5-6d60-477e-bf66-98b0d752e7d4 flake.nix
+        if ! "$grep" --quiet --no-messages --fixed-strings 102363c5-6d60-477e-bf66-98b0d752e7d4 flake.nix
         then
           echo "You must run this in the root of config!" >&2
           exit 1
@@ -29,7 +33,7 @@
           sudo=sudo
         fi
 
-        $sudo nixos-rebuild "$cmd" --flake .
+        $sudo "$nixos_rebuild" "$cmd" --flake .#darter6
       '').outPath;
     };
 
--- nix.nix
+++ nix.nix
@@ -3,8 +3,11 @@
     maxJobs = 8;
     buildCores = 0;
     useSandbox = true;
-    binaryCaches = [ "https://cache.iog.io" ];
-    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    binaryCaches = [ "https://cache.iog.io" "https://shlevy.cachix.org" ];
+    binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "shlevy.cachix.org-1:m0i9ZqalXsWjEkdJT8+hX+Ev7sGnYOPA+DHAS9B6mPo="
+    ];
     trustedUsers = [ "shlevy" ];
     extraOptions = ''
       builders-use-substitutes = true
