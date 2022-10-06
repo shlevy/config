dc7b434182fbdf9a71cc8cc316ed22a942357f6f
Shea Levy <shea@shealevy.com>
2022-10-06 06:01:29 -0400

    Agenda

--- org.nix
+++ org.nix
@@ -6,6 +6,16 @@
       extraConfig = ''
         (require 'org-bullets)
         (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+        (define-key global-map "\C-cl" 'org-store-link)
+        (define-key global-map "\C-ca" 'org-agenda)
+
+        (setq org-agenda-custom-commands '(("n" "Agenda and active TODOs" ((agenda "") (todo "TODO" ((org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))))))))
+      '';
+
+      extraCustomize = ''
+        (customize-set-variable 'org-agenda-files '("~/Documents/roam/planning"))
+        (customize-set-variable 'org-log-into-drawer t)
       '';
     };
   };
