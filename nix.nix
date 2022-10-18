1d4edfc12604a67167b17f0ef66f338a1d91cb3a
Shea Levy <shea@shealevy.com>
2022-10-18 19:01:18 -0400

    Add default nix develop prompt

--- nix.nix
+++ nix.nix
@@ -11,6 +11,7 @@
     trusted-users = [ "root" "shlevy" ];
     builders-use-substitutes = true;
     experimental-features = [ "nix-command" "flakes" ];
+    bash-prompt = ''\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w \[\033[01;31m\](dev-shell)\[\033[01;32m\]]\$\[\033[0m\] '';
   };
 
   home-manager.users.shlevy.programs.emacs.extraPackages = epkgs: [
