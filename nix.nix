af2aa95d72b3b193eba62bb60e29e2eb656adbd2
Shea Levy <shea@shealevy.com>
2022-10-18 08:46:48 -0400

    Use update nix config

--- nix.nix
+++ nix.nix
@@ -1,18 +1,16 @@
 {
-  nix = {
-    maxJobs = 8;
-    buildCores = 0;
-    useSandbox = true;
-    binaryCaches = [ "https://cache.iog.io" "https://shlevy.cachix.org" ];
-    binaryCachePublicKeys = [
+  nix.settings = {
+    max-jobs = 8;
+    cores = 0;
+    sandbox = true;
+    substituters = [ "https://cache.iog.io" "https://shlevy.cachix.org" ];
+    trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "shlevy.cachix.org-1:m0i9ZqalXsWjEkdJT8+hX+Ev7sGnYOPA+DHAS9B6mPo="
     ];
-    trustedUsers = [ "shlevy" ];
-    extraOptions = ''
-      builders-use-substitutes = true
-      experimental-features = nix-command flakes
-    '';
+    trusted-users = [ "root" "shlevy" ];
+    builders-use-substitutes = true;
+    experimental-features = [ "nix-command" "flakes" ];
   };
 
   home-manager.users.shlevy.programs.emacs.extraPackages = epkgs: [
