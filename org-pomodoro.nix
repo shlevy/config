6135c9514632b808c78538a373d640981eb941c8
Shea Levy <shea@shealevy.com>
2022-10-27 07:28:20 -0400

    Sounds

--- org-pomodoro.nix
+++ org-pomodoro.nix
@@ -13,6 +13,12 @@
         (customize-set-variable 'org-pomodoro-length 30)
         (customize-set-variable 'org-pomodoro-short-break-length 3)
         (customize-set-variable 'org-pomodoro-long-break-length 10)
+        (customize-set-variable 'org-pomodoro-ticking-sound-states '(:pomodoro))
+        (customize-set-variable 'org-pomodoro-start-sound "~/Music/reception-bell.wav")
+        (customize-set-variable 'org-pomodoro-finished-sound "~/Music/reception-bell.wav")
+        (customize-set-variable 'org-pomodoro-overtime-sound "~/Music/reception-bell.wav")
+        (customize-set-variable 'org-pomodoro-short-break-sound "~/Music/reception-bell.wav")
+        (customize-set-variable 'org-pomodoro-long-break-sound "~/Music/reception-bell.wav")
       '';
     };
   };
