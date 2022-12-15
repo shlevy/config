431d143f299d87c479506eb9d5a4dd78b6a3cf2b
Shea Levy <shea@shealevy.com>
2022-12-15 12:35:54 -0500

    Add Moritz's cache

--- nix.nix
+++ nix.nix
@@ -3,10 +3,11 @@
     max-jobs = 8;
     cores = 0;
     sandbox = true;
-    substituters = [ "https://cache.iog.io" "https://shlevy.cachix.org" ];
+    substituters = [ "https://cache.iog.io" "https://cache.zw3rk.com" "https://shlevy.cachix.org" ];
     trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "shlevy.cachix.org-1:m0i9ZqalXsWjEkdJT8+hX+Ev7sGnYOPA+DHAS9B6mPo="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
     ];
     trusted-users = [ "root" "shlevy" ];
     builders-use-substitutes = true;
