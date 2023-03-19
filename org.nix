c19a5aace43891bb86f5764cde863d012a6128df
Shea Levy <shea@shealevy.com>
2023-03-19 09:59:22 -0400

    Cleanup

--- org.nix
+++ org.nix
@@ -24,7 +24,7 @@
       '';
 
       extraCustomize = ''
-        (customize-set-variable 'org-agenda-files '("~/Documents/roam/project"))
+        (customize-set-variable 'org-agenda-files '("~/Documents/roam/project" "~/Documents/roam/project/to-clean"))
         (customize-set-variable 'org-log-into-drawer t)
         (customize-set-variable 'org-columns-default-format "%40ITEM(Task) %17Effort(Estimated Effort){:} %CLOCKSUM %CLOCKSUM_T")
       '';
