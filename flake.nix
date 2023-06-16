c9dffebaff66784e43e3fd5426f97958fe01208c
Shea Levy <shea@shealevy.com>
2023-06-16 16:57:12 -0400

    home-manager: Switch to release branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
