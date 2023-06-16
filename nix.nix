b7c9991c5f395dcabd43547b24a8b6d2a74865ec
Shea Levy <shea@shealevy.com>
2023-06-16 06:06:12 -0400

    Add Tweag build machines and fix NIX_PATH

--- nix.nix
+++ nix.nix
@@ -16,6 +16,27 @@
   };
   nix.package = pkgs.nixVersions.nix_2_15;
 
+  nix.nixPath = [ "nixpkgs=/home/shlevy/.nix-defexpr/channels/nixos" ];
+
+  nix.buildMachines = [
+    {
+      hostName = "build01.tweag.io";
+      maxJobs = 24;
+      sshUser = "nix";
+      sshKey = "/home/shlevy/creds/ssh/id-tweag-builder";
+      system = "x86_64-linux";
+      supportedFeatures = [ "big-parallel" "kvm" "nixos-test" ];
+    }
+    {
+      hostName = "build02.tweag.io";
+      maxJobs = 24;
+      sshUser = "nix";
+      sshKey = "/home/shlevy/creds/ssh/id-tweag-builder";
+      systems = ["aarch64-darwin" "x86_64-darwin"];
+      supportedFeatures = [ "big-parallel" ];
+    }
+  ];
+
   home-manager.users.shlevy.programs.emacs.extraPackages = epkgs: [
     epkgs.nix-mode
   ];
