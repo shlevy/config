35ddeb75a11faf5ce84c3c8c5bc4decbdb940081
Shea Levy <shea@shealevy.com>
2024-07-26 13:52:11 -0400

    Add HTTPS git server

--- git.nix
+++ git.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: let
+  git = pkgs.gitAndTools.gitFull;
+in {
   home-manager.users.shlevy.programs = {
     git = {
       enable = true;
 
-      package = pkgs.gitAndTools.gitFull;
+      package = git;
 
       userName = "Shea Levy";
       userEmail = "shea@shealevy.com";
@@ -33,4 +34,39 @@
 
     emacs.extraPackages = epkgs: [ epkgs.magit ];
   };
+
+  services.fcgiwrap = {
+    enable = true;
+    user = "nginx";
+    group = "nginx";
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."git.shealevy.com" = {
+      forceSSL = true;
+      enableACME = true;
+
+      root = "/var/lib/git";
+      locations."/" = {
+        basicAuthFile = "/var/lib/git/auth";
+        fastcgiParams = {
+          SCRIPT_FILENAME = "${git}/libexec/git-core/git-http-backend";
+          GIT_PROJECT_ROOT = "/var/lib/git";
+          PATH_INFO = "$fastcgi_path_info";
+        };
+        extraConfig = ''
+          fastcgi_split_path_info ^()(.*)$;
+          fastcgi_pass unix:/run/fcgiwrap.sock;
+        '';
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "shea@shealevy.com";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
