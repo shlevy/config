70ba13ed096328335b3a3d90e293b8b35a6d8080
Shea Levy <shea@shealevy.com>
2024-07-27 10:37:21 -0400

    D'oh

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     };
   };
   outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosModules.default = [ ./nixos-common.nix ];
+    nixosModules.default = ./nixos-common.nix;
     nixosConfigurations.carbon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit home-manager; };
